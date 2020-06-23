a63ac5d4b3b6417c5b3366056c04d68e4946b7ae
SoengSaravit <soengsaravit@gmail.com>
2020-06-23 22:33:36 +0900

    configue dynamic timezone

--- src/main/java/com/example/ams/SpringBootAmsApplication.java
+++ src/main/java/com/example/ams/SpringBootAmsApplication.java
@@ -2,7 +2,6 @@ package com.example.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
@@ -11,5 +10,12 @@ public class SpringBootAmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAmsApplication.class, args);
+    }
+
+    @PostConstruct
+    private void init(){
+        String timezone = TimeZone.getDefault().getID();
+        System.out.println(timezone);
+        System.setProperty("spring.jackson.time-zone",timezone);
     }
 }
